3b38ee308eee0ecb8a29226460c22d7bec135ded
Shuaib Shaban <shabanshuaib454@gmail.com>
2025-04-15 14:21:11 +0300

    Active text gem added

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,19 @@
 class Employee < ApplicationRecord
-end
+
+    has_rich_text :notes
+    attr_accessor :editable
+  
+    belongs_to :country
+    belongs_to :department
+  
+    scope :search, ->(query) {
+      if query.present?
+        left_joins(:department)
+        .where("employees.id::text LIKE ? OR firstname LIKE ? OR lastname LIKE ? OR departments.name LIKE ?", 
+               "%#{query}%", "%#{query}%", "%#{query}%", "%#{query}%")
+      else
+        all
+      end
+    }
+  end
+  
